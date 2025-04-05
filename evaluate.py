88ddba9e866e9ab583f52f686445c462ad1b028b
Danny Duong <dannyduong4.00@outlook.com>
2025-04-04 20:27:47 -0600

    Overwrite eval results

--- evaluate.py
+++ evaluate.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 # best checkpoint
 model = YOLO(
     "runs/detect/yolov8_chess/weights/best.pt")
-metrics = model.val()  # runs validation on the validation set
+metrics = model.val(exist_ok=True)  # runs validation on the validation set
